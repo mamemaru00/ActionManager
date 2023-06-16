60b5b6f139039409383cd325f7666397efa4e5d2
mamemaru00 <k.shouhei0711@gmail.com>
2023-06-16 15:36:21 +0900

    TailwindCSS追加

--- test/tailwind.config.js
+++ test/tailwind.config.js
@@ -17,4 +17,10 @@ module.exports = {
     },
 
     plugins: [require('@tailwindcss/forms')],
+
+    purge: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
 };
