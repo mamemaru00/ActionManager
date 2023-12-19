3a5fdd8f197cc8c8856607c7ebe25d10934627e7
mamemaru00 <k.shouhei0711@gmail.com>
2023-12-19 18:40:00 +0900

    task-fix-5 取引先会社の情報追加

--- test/database/migrations/2023_06_16_092152_project_table.php
+++ test/database/migrations/2023_06_16_092152_project_table.php
@@ -19,7 +19,7 @@ class ProjectTable extends Migration
             $table->string('project_name')->comment('プロジェクト名');
             $table->integer('manager_code')->comment('担当者コード');
             $table->string('manager_name')->comment('担当者名');
-            $table->unsignedBigInteger('trading_company_id')->nullable(); // プロジェクトIDのカラムを追加
+            $table->unsignedBigInteger('trading_company_id')->nullable();
             $table->dateTime('sales_in_charge')->comment('納期');
             $table->string('order_amount')->comment('受注金額');
             $table->dateTime('order_date')->comment('受注日');
