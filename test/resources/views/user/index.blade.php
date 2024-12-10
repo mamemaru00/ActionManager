b8100b88dd674cf8e79979aa3dfdac9f1ceba1b7
shouhei.katsuyama <k.shouhei0711@gmail.com>
2024-12-11 00:49:21 +0900

    修正箇所

--- test/resources/views/user/index.blade.php
+++ test/resources/views/user/index.blade.php
@@ -2,6 +2,7 @@
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{-- 事業所名を直接コントローラから渡す --}}
+            {{-- ここの記述をviewで取得するようにする --}}
             {{ $officeName ?? '' }}
         </h2>
     </x-slot>
